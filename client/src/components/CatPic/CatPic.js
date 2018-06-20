f9144ce06b5426676d591a88f63f21bf2fd822c9
Trisha McCoy <Trishamccoy1@aol.com>
2018-06-20 18:28:50 -0500

    updates

---
+++ client/src/components/CatPic/CatPic.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./CatPic.css";
+
+const CatPic = props => <div className="Cat">{props.children}</div>;
+
+export default CatPic;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav className="navbar navbar-expand-lg navbar-dark">
     <a className="navbar-brand" href="/">
-      PetFolio
+      Pet Purfect
     </a>
   </nav>
 );
