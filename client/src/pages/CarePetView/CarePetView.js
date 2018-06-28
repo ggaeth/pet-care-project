d0e675d179aa7cbd811d03c6dad8055f7dcb2727
Trisha McCoy <trishamccoy1@aol.com>
2018-06-27 20:21:16 -0500

    Added CarePetView page

---
+++ client/src/pages/CarePetView/CarePetView.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { OwnerBtn, CareBtn } from "../../components/Buttons";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import "./Create.css";
+
+class CarePetView extends Component {
+    state = {
+        careGiver: {},
+      };
+    }
+    export default CarePetView;
