bac26bc3ad15ce068807cd08a2cc13660cad20ca
Trisha McCoy <trishamccoy1@aol.com>
2018-07-09 13:58:13 -0500

    info component

--- client/src/components/InfoBox/InfoBox.js
+++ client/src/components/InfoBox/InfoBox.js
@@ -1,45 +1,46 @@
 import React from "react";
 import Container from "../Container/Container";
-import Image from "../../assets/cat.jpeg";
+// import Image from "../../assets/cat.jpeg";
 import "./InfoBox.css";
 
 const InfoBox = props => (
   <div>
     <div className="row">
       <div className="col-12">
-        <h1 className="infoTitle">About Pet Purfect</h1>
+        <h1 className="infoTitle"> About Pet Purfect</h1>
       </div>
     </div>
-    <div className="background">
+    {/* <div className="background"> */}
     <Container>
       
       <div className="row">
-        <div className="col-6">
+        <div className="col-8">
           <h3 className="info">
-          The Custom Pet Care (CPC) application gives pet owners the ability to provide custom 
-                care information to caregivers about their pet while they are away. This information enhances 
-                the care of their pets.  In addition, pet owners can specify special instructions for each pet 
-                and coordinate with the caregiver to complete the status of very important tasks. If the 
-                specified task has not completed with a specified time, the app can send a message to all 
-                parties informing them that an important task is overdue.
-                Caregivers can also benefit from this application by having a repository of information 
-                  about each pet that is in their care. And, they can earn a good reputation from pet owners 
-                  by following all care instructions.
+          <p>All Pets<i className="fas fa-paw"></i> All Care </p>
+          <p>We care about Pets just as much as you do!! </p>
+          We provide pet owners the ability to provide custom care information to caregivers about 
+          their pet while they are away. This information enhances the care of their pets.  
+          In addition, pet owners can specify special instructions for each pet and coordinate with the 
+          caregiver to complete the status of very important tasks. If the specified task has not completed 
+          with a specified time, our app can send a message to all parties informing them that an important 
+          task is overdue.Caregivers can also benefit from this application by having a repository of information 
+          about each pet that is in their care. And, they can earn a good reputation from pet owners 
+          by following all care instructions.
           </h3>
         </div>
-        <div className="col-1" />
+        {/* <div className="col-1" />
         <div className="col-5">
           <img
             src={Image}
             alt="cat"
             className="catpic"
-          />
-        </div>
+          /> */}
+        {/* </div> */}
       </div>
       
     </Container>
     </div>
-  </div>
+  // </div>
 );
 
 
